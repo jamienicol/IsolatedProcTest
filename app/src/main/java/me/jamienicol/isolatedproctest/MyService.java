7d5ad2d974eec69c076807a759dcd10cf8112ac6
Jamie Nicol <jnicol@mozilla.com>
2022-08-06 11:06:07 +0100

    EGL seems to work so far

--- app/src/main/java/me/jamienicol/isolatedproctest/MyService.java
+++ app/src/main/java/me/jamienicol/isolatedproctest/MyService.java
@@ -6,6 +6,14 @@ import android.graphics.SurfaceTexture;
 import android.os.IBinder;
 import android.util.Log;
 
+import javax.microedition.khronos.egl.EGL;
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGL11;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
+
 public class MyService extends Service {
     private static final String LOGTAG = "MyService";
 
@@ -13,7 +21,9 @@ public class MyService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(LOGTAG, "onStartCommand()");
 
-        testSurfaceTexture();
+        // testSurfaceTexture();
+
+        testGL();
 
         Log.d(LOGTAG, "Finished starting service");
         return START_NOT_STICKY;
@@ -31,3 +41,48 @@ public class MyService extends Service {
         final SurfaceTexture st = new SurfaceTexture(0, false);
         Log.d(LOGTAG, "Finished creating SurfaceTexture");
     }
+
+    private void testGL() {
+        final EGL10 egl = (EGL10)EGLContext.getEGL();
+        Log.d(LOGTAG, "Calling eglGetDisplay");
+        EGLDisplay display = egl.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+        Log.d(LOGTAG, "eglGetDisplay returned " + display);
+
+        int[] version = new int[2];
+        Log.d(LOGTAG, "Calling eglInitialize");
+        boolean ret = egl.eglInitialize(display, version);
+        Log.d(LOGTAG, "eglInitialize returned " + ret);
+
+        int numConfigs[] = new int[1];
+        int[] configAttribs = new int[] {
+                EGL10.EGL_RED_SIZE, 8,
+                EGL10.EGL_GREEN_SIZE, 8,
+                EGL10.EGL_BLUE_SIZE, 8,
+                EGL10.EGL_NONE,
+        };
+        Log.d(LOGTAG, "Calling eglChooseConfig (first time)");
+        egl.eglChooseConfig(display, configAttribs, null, 0, numConfigs);
+        Log.d(LOGTAG, "eglChooseConfig (first time) returned " + numConfigs[0] + " configs");
+
+        EGLConfig[] configs = new EGLConfig[numConfigs[0]];
+        Log.d(LOGTAG, "Calling eglChooseConfig (second time)");
+        egl.eglChooseConfig(display, configAttribs, configs, numConfigs[0], numConfigs);
+        Log.d(LOGTAG, "eglChooseConfig (second time) returned " + numConfigs[0] + " configs");
+
+        int[] contextAttribs = new int[] {
+               EGL10.EGL_NONE,
+        };
+        Log.d(LOGTAG, "Calling eglCreateContext");
+        EGLContext context = egl.eglCreateContext(display, configs[0], EGL10.EGL_NO_CONTEXT, contextAttribs);
+        Log.d(LOGTAG, "eglCreateContext returned " + context);
+
+        int[] surfaceAttribs = new int[] {
+                EGL10.EGL_WIDTH, 1024,
+                EGL10.EGL_HEIGHT, 1024,
+                EGL10.EGL_NONE,
+        };
+        Log.d(LOGTAG, "Calling eglCreatePbufferSurface");
+        EGLSurface surface = egl.eglCreatePbufferSurface(display, configs[0], surfaceAttribs);
+        Log.d(LOGTAG, "eglCreatePbufferSurface returned " + surface);
+    }
+}
